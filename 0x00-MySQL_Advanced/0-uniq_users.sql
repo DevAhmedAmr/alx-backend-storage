640919564c81feb249280b69062c340fa9ddbd5c
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-06-15 11:36:48 +0300

    0. We are all unique! (automated command)

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,6 +2,6 @@
 -- Creates a table with unique id-nn,ai,pk; email-str(255) name-str(255)
 CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email varchar(255),
+    email varchar(255) NOT NULL UNIQUE,
     name varchar(255)
 );
