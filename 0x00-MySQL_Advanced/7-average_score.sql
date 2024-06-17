115945ebdeb7d84a556c1bdbd2b9956cb76b1daa
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-06-17 14:46:04 +0300

    7. Average score (automated command)

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,15 +1,22 @@
 -- sql scipt
 
-DELIMITER $$
-
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+DELIMITER $$;
+CREATE PROCEDURE ComputeAverageScoreForUser(
+	IN user_id INT
+)
 BEGIN
-	UPDATE users
-	SET
-	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
-	WHERE id = user_id;
+    DECLARE avg_score DECIMAL(5, 2);
 
-END $$
+
+	SELECT AVG(score)
+	into avg_score
+	FROM corrections 
+	WHERE corrections.user_id = user_id;
+
+	UPDATE users
+	set average_score = avg_score
+	WHERE corrections.user_id = user_id;
+
+END$$
 
 DELIMITER ;
