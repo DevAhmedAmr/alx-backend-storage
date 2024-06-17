f126f25b8f70d752ecc7d7daf180c84dde1b7dfb
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-06-17 11:41:40 +0300

    6. Add bonus (automated command)

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -3,18 +3,15 @@ DELIMITER $$ CREATE PROCEDURE AddBonus(
 	IN project_name VARCHAR(255),
 	IN score INT
 ) BEGIN
-declare project_id INT;
----------------------------------
-select id into project_id
-from project
-where name = project_name;
--------------------------------------
-IF project_id is null then
-insert into project (name)
-values(project_name);
+DECLARE project_id INT;
+SELECT id INTO project_id
+FROM project
+WHERE name = project_name;
+IF project_id IS NULL THEN
+INSERT INTO project (name)
+VALUES (project_name);
 SET project_id = LAST_INSERT_ID();
-end if;
----------------------
-insert into correction (user_id, project_id, score)
-values(user_id, project_id, score);
-end $$ DELIMITER;
+END IF;
+INSERT INTO correction (user_id, project_id, score)
+VALUES (user_id, project_id, score);
+END $$ DELIMITER;
