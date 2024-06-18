f5122e1e359da631ab123d1846166969db05d28b
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-06-18 12:37:39 +0300

    10. Safe divide (automated command)

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -3,7 +3,7 @@
 DROP VIEW IF EXISTS need_meeting;
 
 CREATE VIEW need_meeting  as
-SELECT *
+SELECT name
 FROM students
 WHERE (score < 80) AND (last_meeting is NULL 
-	OR DATEDIFF(CURDATE(), last_meeting) <= 30);
+	OR DATEDIFF(CURDATE(), last_meeting) > 30);
