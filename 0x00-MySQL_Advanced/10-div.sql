34bd85b849f2a57c42267c8c34d3ba18c0964c83
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-06-18 12:08:04 +0300

    10. Safe divide (automated command)

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,8 +1,10 @@
 -- SQL script that creates a function SafeDiv that divides 
 -- (and returns) the first by the second number or returns 0 
 -- if the second number is equal to 0.
-DELIMITER $$;
+
 DROP FUNCTION IF EXISTS SafeDiv;
+
+DELIMITER $$;
 CREATE function SafeDiv( a int ,  b int)
 returns float deterministic
 BEGIN
