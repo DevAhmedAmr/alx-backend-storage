956b53985fa30f1f846a1aac7ad31eac7ad27f5f
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-06-19 11:19:50 +0300

    11. Where can I learn Python? (automated command)

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,4 +4,7 @@
 
 
 def schools_by_topic(mongo_collection, topic):
+    """Python function that returns the list
+                of school having a specific topic"""
+
     return mongo_collection.find({"topics": topic})
