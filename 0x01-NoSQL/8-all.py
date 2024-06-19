cb915040a24a70edab7618251871db0658bd2eb9
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-06-19 10:37:15 +0300

    8. List all documents in Python (automated command)

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+""" List all documents in Python
+
+	"""
+
+
 def list_all(mongo_collection):
+    """ List all documents in Python
+
+        Args:
+                mongo_collection (Collection): collection to get all it's documents
+
+        Returns:
+                dict: all documents
+        """
     return mongo_collection.find()
