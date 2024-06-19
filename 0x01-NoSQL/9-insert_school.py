294ab478975574e2a836d89cf42942a118d61446
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-06-19 10:58:00 +0300

    9. Insert a document in Python (automated command)

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from pymongo.aggregation import Collection
+# from pymongo.aggregation import Collection
 """ inserts a new document in a collection based on kwargs"""
 
 
-def insert_school(mongo_collection: Collection, **kwargs):
+def insert_school(mongo_collection, **kwargs):
     """ function that inserts a new document in a collection based on kwargs """
 
     return mongo_collection.insert_one(kwargs).inserted_id
