ec1ef6c61e313feb1fb3acc0a86e9a27332de5b9
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-06-19 11:03:30 +0300

    9. Insert a document in Python (automated command)

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,4 +3,7 @@
 
 
 def update_topics(mongo_collection, name, topics):
+    """Python function that changes all topics of
+                a school document based on the name"""
+
     mongo_collection.update({"name": name, "topics": topics}, False, True)
