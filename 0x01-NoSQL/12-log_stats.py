6481125128f17521231fcf95c4cc92f0a479e8a9
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-06-19 12:03:44 +0300

    12. Log stats (automated command)

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -16,7 +16,7 @@ if "__main__" == __name__:
 
     nginx_collection = client.logs.nginx
 
-    all = nginx_collection.count_documents()
+    all = nginx_collection.count_documents({})
 
     get = nginx_collection.count_documents({"method": "GET"})
     post = nginx_collection.count_documents({"method": "POST"})
